32f815b8bb49b7927cec8eef2ab96729f0deca43
Moses <t.content012@gmail.com>
2023-03-31 20:44:36 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name, 
+    email: email
+  }
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  };
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(json => {
+      document.body.innerHTML += json.id;
+    })
+    .catch(error => {
+      document.body.innerHTML += error.message;
+    });
+}
